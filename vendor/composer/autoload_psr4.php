0c5ac362e101420ab44074d7ff6345f57f1b3f4a
dengine <denginestudios@gmail.com>
2015-05-31 14:43:40 +0300

    Change namespace

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Phenix\\' => array($baseDir . '/src'),
+    'Phenix\\' => array($baseDir . '/src/Phenix'),
 );
